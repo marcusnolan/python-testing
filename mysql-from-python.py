b11471639d68121633d22aa070733d2d9fae8283
marcusnolan <marcusnolan130@gmail.com>
2021-03-11 14:43:03 +0000

    mysql-from-python.py updated with assistance from tutor

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,8 +1,22 @@
 import os
 import datetime
+import pymysql
 
-    cursor() as cursor:
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
+username = os.getenv('root')
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
+
+try:
+    with connection.cursor() as cursor:
         cursor.execute("""CREATE TABLE IF NOT EXISTS
                           Friends(name char(20), age int, DOB datetime);""")
         # Note that the above will still display a warning (not error) if the
         # table already exists
+finally:
+    connection.close()
